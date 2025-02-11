d3368d6456c4900a74dbe9be8f076cd4449a098e
Shravanikadage <140402019+Shravanikadage@users.noreply.github.com>
2025-02-11 16:32:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ async function startServer() {
 
     console.log("MongoDB Connected");
 
-    const PORT = process.env.PORT || 4000;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (error) {
     console.error("erver startup error:", error);
